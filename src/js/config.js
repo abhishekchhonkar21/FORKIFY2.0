9786941b9add98494b4905e740d0bd84df7cd1c7
Abhishek Singh <95516886+abhishekchhonkar21@users.noreply.github.com>
2023-07-30 13:04:09 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "9092b405-176b-4f39-8c45-809de0d6da11";
+export const MODAL_CLOSE_SEC = 2.5;
